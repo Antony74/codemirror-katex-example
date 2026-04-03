3d79638a7cb70b665894938eae74c29487740d75
Antony74 <akb@akb.me.uk>
2026-04-03 19:33:00 +0100

    Add codemirror

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+import { basicSetup } from 'codemirror';
+import { EditorView } from '@codemirror/view';
+
+const view = new EditorView({
+    doc: 'Start document',
+    parent: document.body,
+    extensions: [basicSetup],
+});
