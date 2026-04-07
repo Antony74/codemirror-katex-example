bb507d1a66c49af64bd9859e4a7574dc9e8ded35
Antony74 <akb@akb.me.uk>
2026-04-07 10:27:34 +0100

    Link to github and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# codemirror-katex-example
+
+[Live version with explanatory text here](https://antony74.github.io/codemirror-katex-example/dist/)
--- copyfiles.sh
+++ copyfiles.sh
@@ -7,3 +7,4 @@ cp src/index.html dist/index.html
 cp src/styles.css dist/styles.css
 cp node_modules/katex/dist/katex.min.css dist/katex.min.css
 cp -r node_modules/katex/dist/fonts/ dist/fonts/ 
+cp node_modules/github-fork-ribbon-css/gh-fork-ribbon.css dist/gh-fork-ribbon.css
